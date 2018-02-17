1d7727e2b7de161a65d93a10e51ce252939b13e4
EC2 Default User <ec2-user@ip-172-31-37-168.us-east-2.compute.internal>
2018-02-17 17:25:04 +0000

    get_mentor_availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -18,7 +18,13 @@ class Kele
     
     def get_me
        response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token }).body 
-       parsed_result = JSON.parse response
-       return parsed_result
+       JSON.parse response
+    end
+    
+    def get_mentor_availability
+       user_info = self.get_me()
+       mentor_id = user_info["current_enrollment"]["mentor_id"]
+       response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token }).body 
+       JSON.parse response
     end
 end
